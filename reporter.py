bb5145fdb80fe58d274d9b32b57d7f2e03bb386f
allannad <65513146+allannad@users.noreply.github.com>
2020-05-31 15:56:18 -0400

    updated with popular products

--- reporter.py
+++ reporter.py
@@ -12,7 +12,7 @@ print("GENERATING SALES REPORT FOR MONTH OF APRIL 2019...")
 
 #import data 
 import pandas as pd
-
+import numpy
 url = 'https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/monthly-sales/sales-201904.csv'
 df = pd.read_csv(url, error_bad_lines=False)
 
@@ -25,33 +25,35 @@ df['monthname'] = pd.to_datetime(df['month'], format='%m').dt.month_name()
 
 #get month name as variable
 month = df.at[0,'monthname']
+
 #get year as variable
 year = df.at[0,'year']
-#monthyear = df.at[0, 'monthyear']
+
 #create revenue column = (units sold * sales price)
 df["revenue"] = df["units sold"] * df["sales price"]
-#sum revenue column
-revenue = df["revenue"].sum() 
 
-#print results
+#sum and format the revenue column
+revenue = df["revenue"].sum() 
+df["formattedrevenue"] = to_usd(revenue)
+
+#print beginning of report
 print("SALES REPORT" + ' ' +  "("+ str(month) + ' ' + str(year)+")")
-#print("SALES REPORT"+ str(monthyear))
 print("TOTAL SALES",to_usd(revenue))
 
 #identify Top selling products:
-#TOP SELLING PRODUCTS:
-#  1. Button-Down Shirt: $6,960.35
-#  2. Super Soft Hoodie: $1,875.00
-#  3. etc.
+#create new df of items with the max units sold
+pdsales = df.groupby(['product'], as_index=False).sum()
+pdsales["revenue"] = pdsales["units sold"] * pdsales["sales price"]
+pdsales["formattedrevenue"] = pdsales["revenue"].apply(to_usd)
 
-#create new list of items with the max units sold
-pdsales = df.groupby(['product']).sum()
-#print(pdsales)
 #sort them by top sellers
 pdsalessorted = pdsales.sort_values(by=['units sold'], ascending=False)
-print(pdsalessorted)
-#iterate through and print the product name and total revenue
+
+#add column with number of rows, to list out later
+pdsalessorted["number"] = numpy.arange(len(pdsalessorted)) + 1
+
+#iterate through and print the number in popularity, product name and total revenue
 print("TOP SELLING PRODUCTS:")
 for index, row in pdsalessorted.iterrows():
-    print(row['product'], row['revenue'])
+    print(row['number'],row['product'],row['formattedrevenue'])
 
